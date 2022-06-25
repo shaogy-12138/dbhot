149ab9e2fa4bbef085355e31f5ff71ccd7cf6679
shaogy-12138 <shaogy12138@163.com>
2022-06-25 10:48:40 +0800

    v1.0

---
+++ dbhot.c
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: dbhot.c
+	> Author: Jason.shao
+	> Mail: shaogy12138@163.com 
+	> Created Time: Sat 25 Jun 2022 10:47:42 AM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+void func1(){
+    retur;
+}
+
+int main(){
+    func1();
+    return 0;
+}
